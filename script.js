4c6f134b56d3ebb537ff9df98ba63122ddeb8c70
Oleks <archer.opendub@gmail.com>
2022-11-07 23:36:46 +0100

    fixed some bugs, still lots of them left. final design is ready, added keyboard support

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //operand functions
 
 const add = function(a, b) {
-    return parseInt(a) + parseInt(b);
+    return parseFloat(a) + parseFloat(b);
 };
 
 const subtract = function(a, b) {
@@ -27,13 +27,14 @@ const operate = function(a, b, operation){
     }else return multiply(a,b)
 };
 
-
 let firstDigit = '';
 let secondDigit = '';
 let operationToPerform = '';
+let storedValue = '';
 
 
 const storeOperation = function (){
+    dot.disabled = false;
     if(firstDigit!='' && operationToPerform!=''){
         secondDigit = storedValue;
         storedValue = '';
@@ -51,50 +52,64 @@ const storeOperation = function (){
 
 
 const display = document.getElementById('displayValue');
+display.style.color = 'gray';
 const clear = document.getElementById('clear');
 
 clear.addEventListener('click', clearDisplay);
 
 function clearDisplay() {
     display.innerText = 0;
+    display.style.color = 'gray';
     firstDigit = '';
     secondDigit = '';
     operationToPerform = '';
     storedValue = '';
 };
 
-
-
-const erase = document.getElementById('CE');
-erase.addEventListener('click',()=>{
+function eraser(){
     display.innerText = display.innerText.slice(0, -1)
     storedValue = storedValue.slice(0, -1)
+}
 
-})
+const erase = document.getElementById('CE');
+erase.addEventListener('click',eraser)
 
 const digits = document.querySelectorAll('.digit');
 
 digits.forEach(digits=>digits.addEventListener('click', print));
 
+const dot = document.getElementById('dot');
+dot.addEventListener('click', print)
 
 const operands = document.querySelectorAll('.operand');
 operands.forEach(operand=>operand.addEventListener('click', storeOperation));
 
 const equal = document.querySelector('.equal');
 
-equal.addEventListener('click', ()=>{
+function equality(){
     secondDigit = storedValue
     display.innerText = Math.round(operate(firstDigit, secondDigit, operationToPerform)*100)/100;
+    if(display.innerText === "Infinity"){
+        display.innerText = "Nice try"
+    }
+    parseFloat(display.innerText).toFixed(2)
     equalize()
-});
 
+}
 
-let storedValue = '';
+equal.addEventListener('click', equality);
 
-function print() {
-    
+function print() {   
+    display.style.color = '#001e33';
     storedValue += this.innerText;
+    if(storedValue.includes('.')){
+        dot.disabled = true
+    }
+    if(display.innerText == '0' && this.innerText != '.'){
+        display.innerText = this.innerText; 
+    }else{
     display.innerText += this.innerText;
+    }
 };
 
 function equalize(){
@@ -104,3 +119,41 @@ function equalize(){
     secondDigit = '';
 
 }
+
+window.addEventListener('keydown', function (e){
+   if(e.key ==='Escape'){
+    clearDisplay()
+   }else if(e.key ==='Backspace'){
+    eraser()
+   }else if(e.key === 'Enter'){
+    equality()
+   }
+   else if(e.key === '/' ||e.key === '*' ||e.key === '-' ||e.key === '+'){
+    dot.disabled = false;
+        if(firstDigit!='' && operationToPerform!=''){
+            secondDigit = storedValue;
+            storedValue = '';
+            firstDigit = Math.round(operate(firstDigit, secondDigit, operationToPerform)*100)/100;
+            operationToPerform = event.key;
+            display.innerText += operationToPerform;
+
+            }else{
+                firstDigit = storedValue;
+                storedValue = '';
+                operationToPerform = event.key;
+                display.innerText += operationToPerform;
+                }
+                }
+    else if(e.key ==='0' ||e.key ==='1' || e.key ==='2' || e.key ==='3' || e.key ==='4' ||
+    e.key ==='5' || e.key ==='6' || e.key ==='7' || e.key ==='8' || e.key ==='9' || e.key ==='.'){
+        display.style.color = '#001e33';
+        storedValue += e.key;
+        if(display.innerText == '0' && event.key != '.'){
+            display.innerText = event.key; 
+    }else{
+        display.innerText += event.key;
+        }
+   }else{
+    return
+   }
+})
